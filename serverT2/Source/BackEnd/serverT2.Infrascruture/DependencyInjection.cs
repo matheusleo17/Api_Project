245a70cba8f242b4aff79144e2ecfe2ac505fd5c
Matheus Fernandes De Souza Leo <Ms961086@gmail.com>
2025-08-25 22:58:31 -0300

    Configurando injeção de dependencia e conexão com o banco

--- serverT2/Source/BackEnd/serverT2.Infrascruture/DependencyInjection.cs
+++ serverT2/Source/BackEnd/serverT2.Infrascruture/DependencyInjection.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using serverT2.Domain.Repository.User;
+using serverT2.Infrascruture.DataAccess;
+using serverT2.Infrascruture.DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +15,20 @@ namespace serverT2.Infrastructure
     {
         public static void AddInfrastructure(this IServiceCollection services)
         {
+            addRepositories(services);
 
         }
+        
+        private static void addDbContext(IServiceCollection services)
+        {
+            var connectionString = "Data Source = (localdb)\\mssqllocaldb; Initial Catalog =ServerT2; Trusted_Connection=True; Encrypt=True; TrustServerCertificate=True;";
+            services.AddDbContext<AppDbContext>(dbContextOptions =>dbContextOptions.UseSqlServer(connectionString));
+        }
+        private static void addRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUserWriteOnlyRespository, UserRepository>();
+            services.AddScoped<IUserReadOnlyRespository, UserRepository>();
+        }
+
     }
 }
