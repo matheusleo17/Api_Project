3ff9e49b54b868c907cee574543e3d4c985885f5
Matheus Fernandes De Souza Leo <Ms961086@gmail.com>
2025-09-03 23:39:34 -0300

    commit -- feature criando projeto de teste

---
+++ serverT2/CommonTesteUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using serverT2.Communication.Requests;
+
+namespace CommonTesteUtilities.Requests
+{
+    public class RequestRegisterUserJsonBuilder
+    {
+        public static RequestRegisterUserJson Build(int passwordLegth = 10)
+        {
+            return new Faker<RequestRegisterUserJson>()
+                .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+                .RuleFor(user => user.Password, (f) => f.Internet.Password(passwordLegth));
+
+        }
+    }
+}
