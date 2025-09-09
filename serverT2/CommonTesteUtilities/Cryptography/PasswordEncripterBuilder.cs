9274f57f228212ae5517fe18fc9a85de34cbdd2b
Matheus Fernandes De Souza Leo <Ms961086@gmail.com>
2025-09-09 00:08:25 -0300

    commit - feature criação de builder

---
+++ serverT2/CommonTesteUtilities/Cryptography/PasswordEncripterBuilder.cs
@@ -0,0 +1,12 @@
+﻿using serverT2.Application.Services.Criptography;
+
+namespace CommonTesteUtilities.Cryptography
+{
+    public class PasswordEncripterBuilder
+    {
+        public static PasswordCryptography Build()
+        {
+            return new PasswordCryptography("abc1234");
+        }
+    }
+}
