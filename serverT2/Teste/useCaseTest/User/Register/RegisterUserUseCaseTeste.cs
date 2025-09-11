edd0432e5f3b8599e828281ac77f1eddb70823ad
Matheus Fernandes De Souza Leo <Ms961086@gmail.com>
2025-09-10 23:05:10 -0300

    commit -- feature configurando mock para testes

--- serverT2/Teste/useCaseTest/User/Register/RegisterUserUseCaseTeste.cs
+++ serverT2/Teste/useCaseTest/User/Register/RegisterUserUseCaseTeste.cs
@@ -1,6 +1,7 @@
 ﻿using CommonTesteUtilities.Cryptography;
 using CommonTesteUtilities.Mapper;
 using CommonTesteUtilities.Requests;
+using CommonTesteUtilities.Repositories;
 using FluentAssertions;
 using Microsoft.Identity.Client;
 using serverT2.Application.UseCases.User.Register;
@@ -12,18 +13,30 @@ namespace useCaseTest.User.Register
         [Fact]
         public async Task Success()
         {
+
             var request = RequestRegisterUserJsonBuilder.Build();
-            var mapper = MapperBuilder.Build();
-            var passwordEncripter = PasswordEncripterBuilder.Build();
-            var useCase = new RegisterUserUseCase(passwordEncripter,mapper);
 
-             var result = await useCase.Execute(request);
+            var useCase = CreateUseCase();
 
-            result.Should().BeNull();
+            var result = await useCase.Execute(request);
+
+            result.Should().NotBeNull();
 
             result.Name.Should().Be(request.Name);
 
 
+        }
+
+        private RegisterUserUseCase CreateUseCase()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            var mapper = MapperBuilder.Build();
+            var passwordEncripter = PasswordEncripterBuilder.Build();
+            var writeOnlyRepository = UserWriteOnlyRespositoryBuilder.Build();
+            var unityOfWork = UnityOfWorkBuilder.Build();
+            var readOnlyRepository = new UserReadOnlyRepositoryBuilder().Build();
+
+            return new RegisterUserUseCase(writeOnlyRepository, readOnlyRepository, mapper, unityOfWork, passwordEncripter);
         }
     }
 }
